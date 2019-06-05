2f6acd7983d77d51f98fc3f4a509f9e63a4583ea
sangaminta <pushpender1239@gmail.com>
2019-06-05 13:44:36 +0530

    This is for routing in different pages via link.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Root from './container/Root';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+            {/* <Route  path ='/' component={Root} /> */}
+            <Root/>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
---
+++ src/container/NavLink.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class NavLink extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
